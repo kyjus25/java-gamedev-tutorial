3f80435655f059441ac27cc84a58e061261cc461
Justin White <kyjus25@gmail.com>
2020-01-01 23:38:49 -0600

    Quicksave: 2 10:17

---
+++ src/Player.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Player extends GameObject {
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+    }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics g) {
+
+    }
+}
